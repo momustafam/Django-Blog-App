bb72ca48e599c8e3ad0ae37d964b04f1baa5a905
Mohammed Mustafa <mohammedmustafa112025@gmail.com>
2024-04-09 04:29:34 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,28 +1,51 @@
 from django.utils.html import format_html
 from django.contrib.auth import get_user_model
 from django.template import Library
+from blog.models import Post
 
 user_model = get_user_model()
 register = Library()
 
 @register.filter
-def author_details(author, current_user=None):
-    # Handle wrong author or current_user datatype
+def author_details(author, current_user):
     if not isinstance(author, user_model):
+        # return empty string as safe default
         return ""
-    # Display the blog's author name on the blog 
+
     if author == current_user:
-        name = format_html("<strong>me</strong>")
-    elif author.first_name and author.last_name:
+        return format_html("<strong>me</strong>")
+
+    if author.first_name and author.last_name:
         name = f"{author.first_name} {author.last_name}"
     else:
         name = f"{author.username}"
-    # Display a link to the email address of the blog's author
+
     if author.email:
         prefix = format_html('<a href="mailto:{}">', author.email)
-        suffix = format_html('</a>')
+        suffix = format_html("</a>")
     else:
-        prefix = ''
-        suffix = ''
+        prefix = ""
+        suffix = ""
 
-    return format_html("{}{}{}", prefix, name, suffix)
+    return format_html('{}{}{}', prefix, name, suffix)
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+    return format_html('</div>')
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col" {}>', extra_classes)
+
+@register.simple_tag
+def endcol():
+    return format_html('</div>')
--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from blog import views
 
 urlpatterns = [
         path('', views.index),
+        path('post/<slug>/', views.post_detail, name="blog-post-detail")
     ]
